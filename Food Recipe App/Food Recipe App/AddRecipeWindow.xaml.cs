6c35173d0352f6931289d8dd19c71d3a7d0f395c
ngduytkim <ngduytkim@gmail.com>
2020-10-22 18:57:44 +0700

    New windows added: AddRecipeWindow and AddStepWindow

---
+++ Food Recipe App/Food Recipe App/AddRecipeWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+//using Microsoft.Win32;
+
+namespace Food_Recipe_App
+{
+    
+    public partial class AddRecipeWindow : Window
+    {
+        private int _stepsNumber;
+        private int _foodID;
+        public AddRecipeWindow()
+        {
+            InitializeComponent();
+
+            _stepsNumber = 0;
+            this.Height = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+        private void Button_AddHeaderImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select a picture";
+            openFileDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+          "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+          "Portable Network Graphic (*.png)|*.png";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Uri uri = new Uri(openFileDialog.FileName, UriKind.Absolute);
+                HeaderImage.Source = new BitmapImage(uri);
+            }
+        }
+
+        private void Button_AddStep_Click(object sender, RoutedEventArgs e)
+        {
+            AddStepWindow _stepWindow = new AddStepWindow();
+            _stepWindow.Owner = this;
+            _stepWindow.AddedStep += (s, args) =>
+            {
+                _stepsNumber++;
+                args.AddedStep.FoodID = _foodID;
+                args.AddedStep.Number = _stepsNumber;
+                ListView_Steps.Items.Add(args.AddedStep);
+            };
+            _stepWindow.ShowDialog();
+
+        }
+
+        private void Button_Complete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_AddVideo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- Food Recipe App/Food Recipe App/Step.cs
+++ Food Recipe App/Food Recipe App/Step.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Food_Recipe_App
 {
-    class Step
+    public class Step
     {
-        private int foodID;
-        private int number;
-        private string content;
-        List<string> imagesPath;
+        private int _foodID;
+        private int _number;
+        private string _content;
+        List<string> _imagesPath;
+
+        public Step(string description)
+        {
+            _imagesPath = new List<string>();
+            _content = description;
+        }
+
+        public Step()
+        {
+            _imagesPath = new List<string>();
+        }
+
+        public int FoodID { get => _foodID; set => _foodID = value; }
+        public int Number { get => _number; set => _number = value; }
+        public string Content { get => _content; set => _content = value; }
+        public List<BitmapImage> ImagesPath
+        {
+            get
+            {
+                List<BitmapImage> bitmapImgList = new List<BitmapImage>();
+
+                foreach (var path in _imagesPath)
+                {
+                    bitmapImgList.Add(new BitmapImage(new Uri(path, UriKind.Absolute)));
+                }
+
+
+                return bitmapImgList;
+            }
+        }
+        public void AddImage(string path)
+        {
+            _imagesPath.Add(path);
+        }
     }
 }
